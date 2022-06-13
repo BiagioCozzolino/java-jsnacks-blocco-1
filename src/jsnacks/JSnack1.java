7c8282acab3ce57fbfe2118b4e14c207c16b0aee
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-13 10:55:59 +0200

    Primo esercizio terminato

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -4,19 +4,28 @@ import java.util.Scanner;
 
 public class JSnack1 {
 	public static void main(String[] args) {
-		
-		int somma = 0;
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Benvenuto alla somma dei 5 numeri!");
 
-		for (int i = 0; i < 5; i++) {
+		int somma = 0;
+		int flag = 0;
+		Scanner scan = new Scanner(System.in);
+		/*
+		 * Con ciclo For
+		 * 
+		 * 
+		 * System.out.println("Benvenuto alla somma dei 5 numeri!");
+		 * 
+		 * for (int i = 0; i < 5; i++) { System.out.print("Inserisci un numero: ");
+		 * somma = somma + scan.nextInt(); }
+		 */
+		// Con ciclo while
+
+		System.out.println("Benvenuto alla somma dei 5 numeri!");
+		while (flag < 5) {
 			System.out.print("Inserisci un numero: ");
 			somma = somma + scan.nextInt();
+			flag = flag + 1;
 		}
 		System.out.println(somma);
-
 
 	}
 
