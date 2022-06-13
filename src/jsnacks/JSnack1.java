6560918887de234955a2a9c85d320702d19f63d3
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-13 10:48:56 +0200

    Primo esercizio con ciclo For

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -1,5 +1,23 @@
 package jsnacks;
 
+import java.util.Scanner;
+
 public class JSnack1 {
+	public static void main(String[] args) {
+		
+		int somma = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Benvenuto alla somma dei 5 numeri!");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Inserisci un numero: ");
+			somma = somma + scan.nextInt();
+		}
+		System.out.println(somma);
+
+
+	}
 
 }
